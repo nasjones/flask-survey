1333b864bd928fcc25919d92767fca5f8d90683f
nasjones <nasjonez1@gmail.com>
2021-05-06 16:46:40 -0400

    updated to use session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 import surveys
 from random import choice
@@ -7,7 +7,7 @@ from random import choice
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hush'
 debug = DebugToolbarExtension(app)
-responses = []
+
 current_survey = surveys.surveys["satisfaction"]
 
 
@@ -16,13 +16,20 @@ def home():
     return render_template("home.html", survey=current_survey)
 
 
+@app.route('/initialize', methods=['POST'])
+def initialize():
+    session['responses'] = []
+    return redirect('/question/0')
+
+
 @app.route('/question/<int:num>')
 def question(num):
-    if len(responses) == len(current_survey.questions):
+    answered = len(session['responses'])
+    if answered == len(current_survey.questions):
         return redirect('/thanks')
-    elif num != len(responses):
+    elif num != answered:
         flash("Sorry, you are trying to access an invalid question.")
-        return redirect('/question/' + str(len(responses)))
+        return redirect('/question/' + str(answered))
 
     curr_question = current_survey.questions[num]
     return render_template("question.html", question=curr_question, title=current_survey.title, num=num)
@@ -30,7 +37,9 @@ def question(num):
 
 @app.route('/answer', methods=['POST'])
 def answer():
+    responses = session['responses']
     responses.append(request.form["choice"])
+    session['responses'] = responses
     num = request.args.get('num')
     if int(num) < len(current_survey.questions):
         return redirect('/question/'+num)
